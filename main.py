760c61fd2271b3a667128529a21bd09856c40eca
Ali506108 <aliduisen77@gmail.com>
2026-09-17 22:19:29 +0500

    pipeline

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ class Solution:
                 res = min(res , counts[ch])
 
         return res
+
+
+
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        left = 0
+        right = max(piles)
+        res = right
+
+        while left <= right:
+            hour = 0
+            mid = (left+right)//2
+
+            for p in piles:
+                hour += (p+mid-1)//2
+
+            if hour < h :
+                res=mid
+                right = mid -1
+            else:
+                left = mid + 1
+
+        return res
